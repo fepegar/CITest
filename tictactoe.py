862f5ad8793338499f2a200312bdfeff9af5d341
Fernando Perez-Garcia <fepegar@gmail.com>
2020-02-06 12:45:27 +0000

    Fix all bugs

--- tictactoe.py
+++ tictactoe.py
@@ -14,17 +14,48 @@ class Game:
 
     def __init__(self, randomize=False):
         if randomize:
-            self.board = np.random.randint(0, 3, size=(3, 3), dtype=int)
+            while True:
+                self.board = np.random.randint(0, 3, size=(3, 3), dtype=int)
+                if self.is_valid_board():
+                    break
         else:
             self.board = np.zeros((3, 3), dtype=int)
 
+    @staticmethod
+    def parse_position(r, c):
+        possible = 0, 1, 2
+        if r not in possible or c not in possible:
+            raise ValueError('Row and column must be in [0, 2]')
+
     def get_position(self, r, c):
+        self.parse_position(r, c)
         return self.board[r, c]
 
     def place(self, r, c, player):
         """Place the player's marker, where `player` is EX or OH, at row `r` and column `c`."""
         # your code here
+        if player not in (self.EX, self.OH):
+            raise ValueError('Player must be one of (1, 2)')
+        self.parse_position(r, c)
+        if self.get_position(r, c):
+            raise ValueError(f'Position ({r}, {c}) is already taken by player {player}')
         self.board[r, c] = player
+
+    def check_num_plays(self):
+        exes = self.board == self.EX
+        ohs = self.board == self.OH
+        num_exes = exes.sum()
+        num_ohs = ohs.sum()
+        valid_num_plays = abs(num_exes - num_ohs) <= 1
+        return valid_num_plays
+
+    def check_num_wins(self):
+        exes = self.board == self.EX
+        ohs = self.board == self.OH
+        exes_wins = self.get_wins(exes)
+        ohs_wins = self.get_wins(ohs)
+        valid_num_wins = exes_wins + ohs_wins <= 1
+        return valid_num_wins
 
     def is_valid_board(self):
         """
@@ -32,9 +63,17 @@ class Game:
         be in. Eg. since X and O alternate during play the number of each token on the board has to reflect that.
         """
         # your code here
-        num_exes = (self.board == self.EX).sum()
-        num_ohs = (self.board == self.OH).sum()
-        return abs(num_exes - num_ohs) <= 1
+        return self.check_num_plays() and self.check_num_wins()
+
+    @staticmethod
+    def get_wins(player_board):
+        vertical = (player_board.sum(axis=0) == 3).sum()
+        horizontal = (player_board.sum(axis=1) == 3).sum()
+        diag = np.all(np.diag(player_board))
+        diag2 = np.all(np.diag(np.fliplr(player_board)))
+        num_wins = vertical + horizontal + diag + diag2
+        return num_wins
+
 
     def get_winning_player(self):
         """Returns the player number who has won the game, or None if no one has won."""
